7bac743b08059b97f78f22b6c85fa0bf6f57b3d7
hongjungwook1 <110800528+hongjungwook1@users.noreply.github.com>
2025-03-06 11:44:11 +0900

    [Silver II] Title: 최대 힙, Time: 1304 ms, Memory: 114912 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < N; i++) {
+            int number = scan.nextInt();
+            if (number == 0) {
+                if (queue.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(queue.poll());
+                }
+            } else {
+                queue.offer(number);
+            }
+        }
+    }
+}
