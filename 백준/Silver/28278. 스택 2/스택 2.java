dad27c52e03dbef70d5620629480b80212399c66
hongjungwook1 <110800528+hongjungwook1@users.noreply.github.com>
2025-03-07 10:40:01 +0900

    [Silver IV] Title: 스택 2, Time: 2144 ms, Memory: 347924 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int N = scan.nextInt();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+            int number = scan.nextInt();
+
+            if (number == 1) {
+                int num = scan.nextInt();
+                stack.push(num);
+            } else if (number == 2) {
+                if (stack.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(stack.pop()).append("\n");
+                }
+            } else if (number == 3) {
+                sb.append(stack.size()).append("\n");
+            } else if (number == 4) {
+                if (stack.isEmpty()) {
+                    sb.append(1).append("\n");
+                } else {
+                    sb.append(0).append("\n");
+                }
+            } else if (number == 5) {
+                if (stack.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(stack.peek()).append("\n");
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
