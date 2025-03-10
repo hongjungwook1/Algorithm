54bfa3e427b0c3da0e8672167aa1ab3314d2ac41
hongjungwook1 <110800528+hongjungwook1@users.noreply.github.com>
2025-03-10 23:10:25 +0900

    [Bronze V] Title: 사파리월드, Time: 168 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        long N = scan.nextInt();
+        long M = scan.nextInt();
+
+        System.out.println(Math.abs(N - M));
+    }
+}
