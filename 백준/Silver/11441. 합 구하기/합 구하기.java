9b51cd75b289f439748cc68b00488523d68df999
hongjungwook1 <110800528+hongjungwook1@users.noreply.github.com>
2025-03-21 11:31:25 +0900

    [Silver III] Title: 합 구하기, Time: 1240 ms, Memory: 246320 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int N = scan.nextInt();
+
+        int[] prefixSum = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            prefixSum[i] = prefixSum[i - 1] + scan.nextInt();
+        }
+
+        int M = scan.nextInt();
+
+        for (int i = 0; i < M; i++) {
+            int first = scan.nextInt();
+            int end = scan.nextInt();
+            sb.append(prefixSum[end] - prefixSum[first - 1]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
