a81e8b9e5f48e9131c6c9167f6e6ced6e447d569
hongjungwook1 <110800528+hongjungwook1@users.noreply.github.com>
2025-12-09 13:59:44 +0900

    [Bronze II] Title: 이상한 곱셈, Time: 304 ms, Memory: 19664 KB -BaekjoonHub

---
+++ 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+
+        long result = 0;
+
+        for (int i = 0; i < A.length(); i++) {
+            for (int j = 0; j < B.length(); j++) {
+                result += (long) (A.charAt(i) - '0') * (B.charAt(j) - '0');
+            }
+        }
+
+        System.out.println(result);
+    }
+}
